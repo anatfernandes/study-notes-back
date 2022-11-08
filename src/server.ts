4f554df3f2ebbd2519498d0a74c6fba35fad8dcc
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-11-08 16:17:25 -0300

    add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get("/status", (req: Request, res: Response): void => {
 	res.send("It's alive!!!");
 });
 
-server.listen(4000, (): void => console.log(`Listening on port 4000...`));
+server.listen(process.env.PORT, (): void =>
+	console.log(`Listening on port ${process.env.PORT}...`)
+);
